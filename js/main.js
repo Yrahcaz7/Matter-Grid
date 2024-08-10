09c5bfc08b8e82e16855a2a3d488ecfd9b24acd2
Yrahcaz7 <zachary.shive@gmail.com>
2024-08-09 21:49:37 -0400

    add grid & matter

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const TABS = ["Stats", "Settings"];
 
 let game = {
-	grid: [[]],
+	grid: [],
+	layer: [[0, 0]],
 	tab: TABS[0],
 };
 
@@ -15,20 +16,90 @@ function changeTab(index) {
 };
 
 /**
- * Updates the HTML of the right bar.
+ * Gets the player's click power.
+ */
+function getClickPower() {
+	let power = 0.1;
+	return 0.1;
+};
+
+function clickNode(row, col) {
+	let value = game.grid[0][row][col] + getClickPower();
+	game.grid[0][row][col] = Math.min(Math.round(value * 1e12) / 1e12, 1);
+	update();
+};
+
+/**
+ * Updates the HTML of the page.
  */
 function update() {
-	let tabs = "";
+	let html = "";
+	html += "<div id='grid'><table>";
+	while (!game.grid[game.layer.length]) {
+		let arr = [];
+		for (let row = 0; row < 12; row++) {
+			arr[row] = [];
+			for (let col = 0; col < 12; col++) {
+				arr[row][col] = 0;
+			};
+		};
+		if (game.grid.length) arr[0][0] = -1;
+		game.grid.push(arr);
+	};
+	let tier = 0;
+	for (; tier < game.layer.length; tier++) {
+		if (game.layer[tier].length) break;
+	};
+	for (let col = 0; col < 12; col++) {
+		html += "<tr>";
+		for (let row = 0; row < 12; row++) {
+			let prog = 1;
+			if (game.grid[tier + 1][game.layer[tier][0]][game.layer[tier][1]] == -1) {
+				prog = game.grid[tier][row][col];
+			};
+			html += "<td" + (prog < 1 ? " onclick='" + (tier == 0 ?
+				"clickNode(" + row + ", " + col + ")"
+				: "game.layer[" + (tier - 1) + "] = [" + row + ", " + col + "]"
+			) + "' style='cursor: pointer'" : "") + ">";
+			if (prog > 0) html += "<div" + (prog < 1 ? " style='width: " + (prog * 100) + "%'" : "") + "></div>";
+			html += "</td>";
+		};
+		html += "</tr>";
+	};
+	html += "</table></div><div id='bar'><div id='tabs'>";
 	for (let index = 0; index < TABS.length; index++) {
-		if (TABS[index] == game.tab) tabs += "<button class='on'>" + TABS[index] + "</button>";
-		else tabs += "<button onclick='changeTab(" + index + ")'>" + TABS[index] + "</button>";
+		if (TABS[index] == game.tab) html += "<button class='on'>" + TABS[index] + "</button>";
+		else html += "<button onclick='changeTab(" + index + ")'>" + TABS[index] + "</button>";
 	};
-	document.getElementById("tabs").innerHTML = tabs;
-	let main = "";
-	if (game.tab == "Settings") {
-		main += "<button onclick='SAVE.wipe()'>Wipe Save</button><button onclick='SAVE.export()'>Export Save</button><button onclick='SAVE.import()'>Import Save</button><button onclick='SAVE.save()'>Save Game</button>";
+	html += "</div><div id='main'>";
+	if (game.tab == "Stats") {
+		let regions = [];
+		for (let tier = 0; tier < game.grid.length; tier++) {
+			regions[tier] = 0;
+			for (let row = 0; row < 12; row++) {
+				for (let col = 0; col < 12; col++) {
+					if (game.grid[tier][row][col] == 1) regions[tier]++;
+				};
+			};
+		};
+		let matter = 0;
+		for (let index = 0; index < regions.length; index++) {
+			matter += regions[index] * (144 ** index);
+		};
+		html += "You have a total of " + matter + " matter, which is made out of:";
+		for (let index = 0; index < regions.length; index++) {
+			if (index == 0) html += "<br>" + regions[index] + " stray matter";
+			else html += "<br>" + regions[index] + " type-" + String.fromCharCode(64 + index) + " regions";
+		};
+	} else if (game.tab == "Settings") {
+		html += "Saving Settings<hr>";
+		html += "<button onclick='SAVE.wipe()'>Wipe Save</button>";
+		html += "<button onclick='SAVE.export()'>Export Save</button>";
+		html += "<button onclick='SAVE.import()'>Import Save</button>";
+		html += "<button onclick='SAVE.save()'>Save Game</button>";
 	};
-	document.getElementById("main").innerHTML = main;
+	html += "</div></div>";
+	document.body.innerHTML = html;
 };
 
 window.addEventListener("load", () => {
