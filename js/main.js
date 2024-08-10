d7f93c183734349f47abea9c9208f34e0237a9aa
Yrahcaz7 <zachary.shive@gmail.com>
2024-08-10 01:52:07 -0400

    add layer navigation and many other things

--- js/main.js
+++ js/main.js
@@ -16,6 +16,54 @@ function changeTab(index) {
 };
 
 /**
+ * Gets the starting state of a normal layer.
+ */
+function getStartLayer() {
+	let arr = [];
+	for (let row = 0; row < 12; row++) {
+		arr[row] = [];
+		for (let col = 0; col < 12; col++) {
+			arr[row][col] = 0;
+		};
+	};
+	return arr;
+};
+
+/**
+ * Goes up to the specified tier.
+ * @param {number} tier - the tier to go to.
+ */
+function goUpToTier(tier) {
+	if (!game.layer[tier]) game.layer[tier] = [0, 0];
+	for (let index = 0; index < tier; index++) {
+		game.layer[index] = [];
+	};
+	update();
+};
+
+/**
+ * Completes the current layer specified by its tier.
+ * @param {number} tier - the tier of the layer.
+ */
+function completeLayer(tier) {
+	game.grid[tier] = getStartLayer();
+	game.grid[tier + 1][game.layer[tier][0]][game.layer[tier][1]] = 1;
+	goUpToTier(tier + 1);
+};
+
+/**
+ * Enters the layer specified by its tier, row, and column.
+ * @param {number} tier - the tier of the layer.
+ * @param {number} row - the row of the layer.
+ * @param {number} col - the column of the layer.
+ */
+function enterLayer(tier, row, col) {
+	if (game.grid[tier + 1][row][col] < 1) game.grid[tier + 1][row][col] = -1;
+	game.layer[tier] = [row, col];
+	update();
+};
+
+/**
  * Gets the player's click power.
  */
 function getClickPower() {
@@ -23,6 +71,11 @@ function getClickPower() {
 	return 0.1;
 };
 
+/**
+ * Clicks the node with the specified row and column in the active tier 0 layer.
+ * @param {number} row - the row of the node to click.
+ * @param {number} col - the column of the node to click.
+ */
 function clickNode(row, col) {
 	let value = game.grid[0][row][col] + getClickPower();
 	game.grid[0][row][col] = Math.min(Math.round(value * 1e12) / 1e12, 1);
@@ -34,15 +87,9 @@ function clickNode(row, col) {
  */
 function update() {
 	let html = "";
-	html += "<div id='grid'><table>";
+	html += "<div id='grid' oncopy='return false' onpaste='return false' oncut='return false'><table>";
 	while (!game.grid[game.layer.length]) {
-		let arr = [];
-		for (let row = 0; row < 12; row++) {
-			arr[row] = [];
-			for (let col = 0; col < 12; col++) {
-				arr[row][col] = 0;
-			};
-		};
+		let arr = getStartLayer();
 		if (game.grid.length) arr[0][0] = -1;
 		game.grid.push(arr);
 	};
@@ -50,19 +97,64 @@ function update() {
 	for (; tier < game.layer.length; tier++) {
 		if (game.layer[tier].length) break;
 	};
-	for (let col = 0; col < 12; col++) {
+	for (let col = -1; col < 12; col++) {
 		html += "<tr>";
-		for (let row = 0; row < 12; row++) {
-			let prog = 1;
-			if (game.grid[tier + 1][game.layer[tier][0]][game.layer[tier][1]] == -1) {
-				prog = game.grid[tier][row][col];
+		for (let row = -1; row < 12; row++) {
+			if (row < 0 && col < 0) {
+				let complete = true;
+				check: for (let r = 0; r < 12; r++) {
+					for (let c = 0; c < 12; c++) {
+						if (game.grid[tier][r][c] < 1) {
+							complete = false;
+							break check;
+						};
+					};
+				};
+				if (complete)  html += "<td onclick='completeLayer(" + tier + ")' style='cursor: pointer'>&larr;</td>";
+				else if (game.grid[tier + 1][0][0] == 1) html += "<td onclick='goUpToTier(" + (tier + 1) + ")' style='cursor: pointer'>&larr;</td>";
+				else html += "<td>&nbsp;</td>";
+			} else if (row < 0) {
+				let prog = 1;
+				if (game.grid[tier + 1][game.layer[tier][0]][game.layer[tier][1]] == -1) {
+					prog = 0;
+					for (let r = 0; r < 12; r++) {
+						if (game.grid[tier][r][col] == 1) prog++;
+					};
+					prog /= 12;
+				};
+				if (prog > 0) html += "<th scope='col'><div" + (prog < 1 ? " style='width: " + (prog * 100) + "%'" : "") + ">&nbsp;</div></th>";
+				else html += "<th scope='col'>&nbsp;</th>";
+			} else if (col < 0) {
+				let prog = 1;
+				if (game.grid[tier + 1][game.layer[tier][0]][game.layer[tier][1]] == -1) {
+					prog = 0;
+					for (let c = 0; c < 12; c++) {
+						if (game.grid[tier][row][c] == 1) prog++;
+					};
+					prog /= 12;
+				};
+				if (prog > 0) html += "<th><div" + (prog < 1 ? " style='width: " + (prog * 100) + "%'" : "") + ">&nbsp;</div></th>";
+				else html += "<th>&nbsp;</th>";
+			} else {
+				let prog = 1;
+				if (game.grid[tier][row][col] == -1) {
+					prog = 0;
+					for (let r = 0; r < 12; r++) {
+						for (let c = 0; c < 12; c++) {
+							if (game.grid[tier - 1][r][c] == 1) prog++;
+						};
+					};
+					prog /= 144;
+				} else if (game.grid[tier + 1][game.layer[tier][0]][game.layer[tier][1]] == -1) {
+					prog = game.grid[tier][row][col];
+				};
+				html += "<td" + (tier == 0 ?
+					(prog < 1 ? " onclick='clickNode(" + row + ", " + col + ")' style='cursor: pointer'" : "")
+					: " onclick='enterLayer(" + (tier - 1) + ", " + row + ", " + col + ")' style='cursor: pointer'"
+				) + ">";
+				if (prog > 0) html += "<div" + (prog < 1 ? " style='width: " + (prog * 100) + "%'" : "") + "></div>";
+				html += "</td>";
 			};
-			html += "<td" + (prog < 1 ? " onclick='" + (tier == 0 ?
-				"clickNode(" + row + ", " + col + ")"
-				: "game.layer[" + (tier - 1) + "] = [" + row + ", " + col + "]"
-			) + "' style='cursor: pointer'" : "") + ">";
-			if (prog > 0) html += "<div" + (prog < 1 ? " style='width: " + (prog * 100) + "%'" : "") + "></div>";
-			html += "</td>";
 		};
 		html += "</tr>";
 	};
@@ -90,6 +182,17 @@ function update() {
 		for (let index = 0; index < regions.length; index++) {
 			if (index == 0) html += "<br>" + regions[index] + " stray matter";
 			else html += "<br>" + regions[index] + " type-" + String.fromCharCode(64 + index) + " regions";
+		};
+		html += "<br>";
+		let runningTotal = 0;
+		for (let row = 0; row < 12; row++) {
+			for (let col = 0; col < 12; col++) {
+				if (game.grid[0][row][col] > 0) runningTotal += game.grid[0][row][col];
+			};
+		};
+		for (let index = 1; index < regions.length; index++) {
+			html += "<br>You are ~" + (runningTotal / (144 ** index) * 100).toFixed(2) + "% of the way to filling a type-" + String.fromCharCode(64 + index) + " region";
+			runningTotal += regions[index] * (144 ** index);
 		};
 	} else if (game.tab == "Settings") {
 		html += "Saving Settings<hr>";
